0060eedf0eacd22f84eee63f98e3d3e7641add52
licong <licong@xiangrikui.com>
2018-09-18 11:11:55 +0800

    Initial Commit

---
+++ testXcodeServe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  testXcodeServe
+//
+//  Created by allen_dev on 2018/9/18.
+//  Copyright © 2018 lc_allen_dev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
